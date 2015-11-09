c4cf58b258a7a3c3fe6225deee82b402a771c916
AlexJoz <yakimbee@gmail.com>
2015-11-09 12:17:07 +0200

    OutCast init

---
+++ Week6-WordNet/Outcast.java
@@ -0,0 +1,13 @@
+public class Outcast {
+   public Outcast(WordNet wordnet){
+       // constructor takes a WordNet object
+   }
+   
+   public String outcast(String[] nouns){
+    return null;
+       // given an array of WordNet nouns, return an outcast
+   }
+   public static void main(String[] args){
+       // see test client below
+   }
+}
